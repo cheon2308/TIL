1cc7fdbae3d9823426d85017f9131ff73afc268d
cheon2308 <cjsgusxo95@gmail.com>
2022-07-18 11:23:42 +0900

    DAY1 | MID SAVE

---
+++ python/day1_data.py
@@ -0,0 +1,11 @@
+name = 'Kim'
+score = 4.5
+
+print(f'Hello, {name}! 성적은 {score}')
+
+import datetime
+today = datetime.datetime.now()
+print(today)
+
+
+print(f'오늘은 {today : %y}년 {today : %m}월 {today : %d}일 입니다')
