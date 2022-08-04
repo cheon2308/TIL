c1bdec53e7e7a3203e2d3d5ac1c94811e35e16af
cheon2308 <cjsgusxo@gmail.com>
2022-08-04 10:21:01 +0900

    before homework

--- swea/1946_부산_0854470_천현태.py
+++
@@ -1,9 +0,0 @@
-for tc in range(1, int(input())+1):
-    answer = ''
-    print(f'#{tc}')
-    for _ in range(int(input())):
-        alpha, n = input().split()
-        n = int(n)
-        answer += alpha*n
-    for i in range(0, len(answer), 10):
-        print(answer[i:i+10])
