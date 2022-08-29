b13e20397cd72539cbce81083032a121f27e0bff
천현태[부울경_1반] <cjsgusxo95@gmail.com>
2022-08-29 22:41:58 +0900

    tired

--- Algorithm/ex.py
+++ Algorithm/ex.py
@@ -1,35 +1,15 @@
-import sys
-from itertools import permutations
-sys.stdin = open('input.txt')
+num = [i for i in range(1,10001)]
 
-T = int(input())
+gen = []
 
-for tc in range(1, T+1):
-    N=int(input())
-    arr = [list(map(int, input().split())) for _ in range(N)]
-    num = ''
-    for i in range(len(arr)):
-        num = ''
-        for j in range(len(arr)):
-            num += str(arr[i][j])
-        arr[i] = num
+for i in range(len(num)):
+    number = int(i)
+    for j in str(i):
+        number += int(j)
+    gen.append(number)
 
-    result = []
-    cnt = 0
-    while cnt != 3:
-        for i in range(len(arr)):
-            num = ''
-            for j in range(len(arr)-1,-1,-1):
-                num += str(arr[j][i])
-            result.append(num)
+for k in set(gen):
+    num.remove(k)
 
-        for k in range(N):
-            arr[k] = result[len(result)-N+k]
-
-        cnt +=1
-
-    print(f'#{tc}')
-    cnt = 0
-
-    for i in range(N):
-        print(result[i], result[i+N], result[i+2*N])
+for l in num:
+    print(l)
