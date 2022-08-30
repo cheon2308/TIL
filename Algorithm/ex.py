4720f30be7c3298ed7ff7f5e97fbd06c1c97df0d
cheon2308 <cjsgusxo95@gmail.com>
2022-08-30 17:32:38 +0900

    dd

--- Algorithm/ex.py
+++ Algorithm/ex.py
@@ -1,8 +1,9 @@
 # A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B
-#
+# 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F
 
-N = list(map(int, input().split()))
-#
-result = (sum(i**2 for i in N))
-#
-print(result%10)
+
+word = input()
+alphabet = list(range(97,123))
+
+for i in alphabet:
+    print(word.find(chr(i)))
